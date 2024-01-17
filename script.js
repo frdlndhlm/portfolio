6a236a827431b0ed5146d78580f3f956a87d7ffa
Frida <frida.lindholm@turku.fi>
2024-01-17 11:26:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const div = document.querySelector(".text");
+const text = "Hello! Welcome to my Portfolio.";
+
+function textTypingEffect(element, text, i = 0) {
+    if (i === 0) {
+        element.textContent = "";
+    }
+    element.textContent += text[i];
+
+    if (i === text.length - 1) {
+        return;
+    }
+
+    setTimeout(() => textTypingEffect(element, text, i + 1), 50);
+}
+
+textTypingEffect(div, text);
